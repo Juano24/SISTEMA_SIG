b8b320058933f10c5214ece1b565d1c7d9d814b7
45duro <jonathangc.awt@gmail.com>
2018-10-22 11:40:01 -0500

    Consultas a la base de Datos Correcta, aùn no se tiene una respuesta en la parte del registro pero ya la mejoraré

--- login/Pruebas/consultando.php
+++ login/Pruebas/consultando.php
@@ -1,12 +1,20 @@
 <?php
 
-$mysqli = new mysqli('server174.hostinger.co', 'u663545672_sig', '123456789', 'u663545672_sig');
+$mysqli = new mysqli('localhost', 'u663545672_sig', '123456789', 'u663545672_sig');
 
 if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
 		exit();
 }
 
+echo "Hola mundo";
 
+if ($resultado = $mysqli->query("SELECT * FROM `tipo_usuario`")) {
+    
+    //printf($resultado);
+    printf("La selección devolvió %d filas.\n", $resultado->num_rows);
 
+    /* liberar el conjunto de resultados */
+    $resultado->close();
+}
 ?>
