ecc81aa3b6364b17bff47f13e0beb1a6dbb3ec98
45duro <jonathangc.awt@gmail.com>
2018-10-23 16:20:56 -0500

    Registro solucionado en server local

--- login/Pruebas/enviarMail.php
+++ login/Pruebas/enviarMail.php
@@ -1,4 +1,6 @@
 <?php
+	
+	require '../funciones/funcs.php';
 
 	$url = 'http://'.$_SERVER["SERVER_NAME"];
 
@@ -6,14 +8,14 @@
 	$asunto = 'Wesleyana Casa De Dios Activate - SISTEMA SIG';
 	$cuerpo = "Estimado líder $nombre: <br /><br /> De Clic en el siguiente enlace para continuar con el proceso de registro  <a href='$url'>Activar Cuenta</a>";
 
-	/*
-	if(enviarEmail($email, $nombre, $asunto, $cuerpo))
-	{
+	
+	if(enviarEmail('jonathangc.awt@gmail.com', 'Jonathan', $asunto, $cuerpo)){
+		
 		echo "Se ha enviado un correo al email proporcionado: $email, para poder disfrutar de todos nuestros beneficios";
 
 		echo "<br><a href='index.php' >Iniciar Sesión</a>";
-	*/
-
+	
+	}
 
 
 ?>
