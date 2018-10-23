a25cf836f5b26d49a6a664e85eeeeddd7348c4b4
45duro <jonathangc.awt@gmail.com>
2018-10-23 16:10:03 -0500

    Registro solucionado en server local

--- login/Pruebas/enviarMail.php
+++ login/Pruebas/enviarMail.php
@@ -1,6 +1,19 @@
 <?php
 
-echo "hola php mailer"
+	$url = 'http://'.$_SERVER["SERVER_NAME"];
+
+	echo $url;
+	$asunto = 'Wesleyana Casa De Dios Activate - SISTEMA SIG';
+	$cuerpo = "Estimado líder $nombre: <br /><br /> De Clic en el siguiente enlace para continuar con el proceso de registro  <a href='$url'>Activar Cuenta</a>";
+
+	/*
+	if(enviarEmail($email, $nombre, $asunto, $cuerpo))
+	{
+		echo "Se ha enviado un correo al email proporcionado: $email, para poder disfrutar de todos nuestros beneficios";
+
+		echo "<br><a href='index.php' >Iniciar Sesión</a>";
+	*/
+
 
 
 ?>
