51606fe7f0e0a4bc1a7a48216a0acd9a79548aca
45duro <jonathangc.awt@gmail.com>
2018-10-23 15:41:59 -0500

    Registro solucionado en server local

--- login/funciones/conexion.php
+++ login/funciones/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
 	//servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-	//$mysqli = new mysqli('localhost', 'u663545672_sig', '123456789', 'u663545672_sig');
+	$mysqli = new mysqli('localhost', 'u663545672_sig', '123456789', 'u663545672_sig');
 
-	$mysqli = new mysqli('localhost', 'root', '', 'login');
+	//$mysqli = new mysqli('localhost', 'root', '', 'login');
 	if(mysqli_connect_errno()){
 		echo 'Conexion Fallida : ', mysqli_connect_error();
 		exit();
