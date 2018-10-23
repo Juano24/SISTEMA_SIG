29a6153dd4aaa98dc5f6c2e8316e7f0cfb41f74e
45duro <jonathangc.awt@gmail.com>
2018-10-23 09:10:07 -0500

    Probando Registro

--- login/Pruebas/consultando.php
+++ login/Pruebas/consultando.php
@@ -1,17 +1,17 @@
 <?php
-/*
-$mysqli = new mysqli('localhost', 'u663545672_sig', '123456789', 'u663545672_sig');
 
-if(mysqli_connect_errno()){
-		echo 'Conexion Fallida : ', mysqli_connect_error();
-		exit();
-}
+	$mysqli = new mysqli('localhost', 'u663545672_sig', '123456789', 'u663545672_sig');
 
-*/
+	if(mysqli_connect_errno()){
+			echo 'Conexion Fallida : ', mysqli_connect_error();
+			exit();
+	}
 
-	require 'funciones/conexion.php';
 
-	global $mysqli;
+
+	//require 'funciones/conexion.php';
+
+	//global $mysqli;
 
 	echo "hola mundo";
 
